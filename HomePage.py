6d01eb7f121c27ac1b6ffb07ee1aa6956433c6e1
zchenchen1999 <zchenchen1999@gmail.com>
2023-05-31 20:11:17 +0800

    first commit

---
+++ HomePage.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# 預設顯示 wide mode
+st.set_page_config(page_title="裕日有望客分析系統", layout="wide")
+
+# title
+st.title("裕日有望客分析系統")
---
+++ pages/1_Word_frequency.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# 預設顯示 wide mode
+st.set_page_config(layout="wide")
+
+# title
+st.title("詞頻分析/文字雲")
