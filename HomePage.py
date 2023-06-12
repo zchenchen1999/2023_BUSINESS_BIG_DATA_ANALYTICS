53379d5e511789eeff1c80ea3f0c595f5d231c7c
zchenchen1999 <zchenchen1999@gmail.com>
2023-06-12 16:57:09 +0800

    add cache

--- HomePage.py
+++ HomePage.py
@@ -12,7 +12,9 @@ st.title("裕日有望客分析系統")
 
 car_brand = ["nissan", "toyota", "ford", "honda", "mazda"]
 
-# 讀取載入 data，若輸入參數在先前 streamlit 已看過，則會直接讀去 cache 中的 data
+# 讀取載入 data，若輸入參數在先前 streamlit 已看過，則會直接讀去 cache 中的結果（不執行 def）
+#              若輸入參數沒看過，則會執行 def
+#              不只有讀取 data 可使用， UI 也可以（如果覺得每次畫圖話很久，可以放在 cache 中的 function 執行）=> 輸入參數沒變會直接顯示之前畫好的圖
 @st.cache_data  # 👈 Add the caching decorator
 def load_data(url):
     try:
@@ -25,7 +27,7 @@ def load_data(url):
         return None
 
 @st.cache_data
-def load_ptt_data(string):
+def show_ptt_data():
     st.header("外部原始資料")
     car_brand_tabs = st.tabs(car_brand)
     for p in range(len(car_brand_tabs)):
@@ -33,60 +35,13 @@ def load_ptt_data(string):
             car_brand_tabs[p].dataframe(data=load_data(f"big-data-class-2023/{car_brand[p]}_ptt_data.csv"), use_container_width=True)
         except:
             print("本機")
-    # try:
-    #     ptt_df_list = []
-    #     ptt_df_list.append(load_data("big-data-class-2023/nissan_ptt_data.csv"))
-    #     ptt_df_list.append(load_data("big-data-class-2023/toyota_ptt_data.csv"))
-    #     ptt_df_list.append(load_data("big-data-class-2023/ford_ptt_data.csv"))
-    #     ptt_df_list.append(load_data("big-data-class-2023/honda_ptt_data.csv"))
-    #     ptt_df_list.append(load_data("big-data-class-2023/mazda_ptt_data.csv"))
-    #     return ptt_df_list
-    # # 本機讀取自己的路徑
-    # except:
-    #     print("本機")
-    #     return None
 
 
-internal = load_data("big-data-class-2023/nissan_internal.csv")
-
-# ptt_df_list = load_ptt_data("ptt")
-# ptt_df_list.append(load_data("big-data-class-2023/nissan_ptt_data.csv"))
-# ptt_df_list.append(load_data("big-data-class-2023/toyota_ptt_data.csv"))
-# ptt_df_list.append(load_data("big-data-class-2023/ford_ptt_data.csv"))
-# ptt_df_list.append(load_data("big-data-class-2023/honda_ptt_data.csv"))
-# ptt_df_list.append(load_data("big-data-class-2023/mazda_ptt_data.csv"))
 
 # 內部資料
 st.header("內部資料")
+# 讀取內部資料
+internal = load_data("big-data-class-2023/nissan_internal.csv")
 st.dataframe(internal)
-load_ptt_data("ptt")
-# 外部 ptt 資料
-# st.header("外部原始資料")
-# car_brand_tabs = st.tabs(car_brand)
-# for p in range(len(ptt_df_list)):
-#     car_brand_tabs[p].dataframe(data=ptt_df_list[p], use_container_width=True)
-
-
-# SECRET = st.secrets["gcp_service_account"]
-
-# # 取得 SECRET 範例： SECRET.get("type")
-
-# # 與 google sheet 連結
-# # Create a connection object.
-# credentials = service_account.Credentials.from_service_account_info(
-#     st.secrets["gcp_service_account"],
-#     scopes=[
-#         "https://www.googleapis.com/auth/spreadsheets",
-#     ],
-# )
-# conn = connect(credentials=credentials)
-
-# def run_query(query):
-#     rows = conn.execute(query, headers=1)
-#     rows = pd.DataFrame(rows.fetchall())
-#     return rows
-
-# sheet_url = st.secrets["private_gsheets_url"]
-# df = run_query(f'SELECT * FROM "{sheet_url}"')
-
-# st.dataframe(df)
+# 顯示外部資料
+show_ptt_data()
