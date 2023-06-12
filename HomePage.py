e0ea0d14836b99b07a958f29dab5d9abaf76e0ec
zchenchen1999 <zchenchen1999@gmail.com>
2023-06-12 18:43:07 +0800

    update HomePage

--- HomePage.py
+++ HomePage.py
@@ -17,7 +17,7 @@ car_brand = ["nissan", "toyota", "ford", "honda", "mazda"]
 #              不只有讀取 data 可使用， UI 也可以（如果覺得每次畫圖話很久，可以放在 cache 中的 function 執行）=> 輸入參數沒變會直接顯示之前畫好的圖
 conn = st.experimental_connection('gcs', type=FilesConnection)
 
-@st.cache_data  # 👈 Add the caching decorator
+# @st.cache_data  # 👈 Add the caching decorator
 def load_data(url):
     try:
         csv_data = conn.read(url, input_format="csv", ttl=1000)
