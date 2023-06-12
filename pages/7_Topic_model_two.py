a2d71a06386df5ff465fba7f7f5e97f5b8469d08
zchenchen1999 <zchenchen1999@gmail.com>
2023-06-13 02:01:01 +0800

    add topic two

---
+++ pages/7_Topic_model_two.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import streamlit.components.v1 as components
+# from st_files_connection import FilesConnection
+
+# 負責人：畇彤
+
+# ---------------------------------------------------------- Streamlit ----------------------------------------------------------#
+
+# 預設顯示 wide mode
+st.set_page_config(layout="wide")
+
+# title
+st.title("主題模型")
+
+# # st.sidebar.header('')
+# path = '/tmp/nissan_lda.html'
+# with open(path, 'r') as f :
+#     HtmlFile = f.read()
+
+# components.html(HtmlFile.read(), height=660, scrolling=True)
