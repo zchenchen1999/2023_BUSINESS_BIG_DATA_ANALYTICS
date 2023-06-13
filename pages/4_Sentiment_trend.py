28e5a94901689b4ba767151a6f2bcf94498cfbb8
zchenchen1999 <zchenchen1999@gmail.com>
2023-06-13 21:35:12 +0800

    update page4

--- pages/4_Sentiment_trend.py
+++ pages/4_Sentiment_trend.py
@@ -99,79 +99,82 @@ st.sidebar.title('選擇正負向文章類別')
 sentiment_list = ['positive', 'negative']
 selected_sentiment = st.sidebar.multiselect('選擇正向或負向類別', sentiment_list, default=['positive'])
 
-# Filter dataframe based on selected sentimentRatio
-if 'positive' in selected_sentiment:
-    df_filtered_sentiment_positive = df_select[df_select['sentimentRatio'] > 0.6]
+if (selected_brands &sentiment_list):
+    # Filter dataframe based on selected sentimentRatio
+    if 'positive' in selected_sentiment:
+        df_filtered_sentiment_positive = df_select[df_select['sentimentRatio'] > 0.6]
+    else:
+        df_filtered_sentiment_positive = pd.DataFrame(columns=df_select.columns)  # 空的 DataFrame
+
+    if 'negative' in selected_sentiment:
+        df_filtered_sentiment_negative = df_select[df_select['sentimentRatio'] < 0.4]
+    else:
+        df_filtered_sentiment_negative = pd.DataFrame(columns=df_select.columns)  # 空的 DataFrame
+
+    df_filtered_sentiment_positive['artDate'] = pd.to_datetime(df_filtered_sentiment_positive['artDate'])
+    df_filtered_sentiment_negative['artDate'] = pd.to_datetime(df_filtered_sentiment_negative['artDate'])
+
+    # Group by brand, artDate, and sentiment, then calculate total volume
+    brand_sentiment_count_positive = df_filtered_sentiment_positive.groupby(['Brand', pd.Grouper(key='artDate', freq='M', sort=False)])['Brand'].count().reset_index(name='count').rename(columns={'count': 'positive_count'})
+
+    brand_sentiment_count_negative = df_filtered_sentiment_negative.groupby(['Brand', pd.Grouper(key='artDate', freq='M', sort=False)])['Brand'].count().reset_index(name='count').rename(columns={'count': 'negative_count'})
+
+
+    # Merge positive and negative counts
+    brand_sentiment_count_merged = pd.merge(brand_sentiment_count_positive, brand_sentiment_count_negative, on=['Brand', 'artDate'], how='outer').fillna(0)
+    # # Pivot the table to have sentimentRatio as columns
+    # brand_sentiment_count_pivot = brand_sentiment_count_merged.pivot_table(index='artDate',
+    #                                                                       columns='Brand',
+    #                                                                       values='count',
+    #                                                                       fill_value=0)
+
+    # # Plot line chart
+    # fig = px.line(brand_sentiment_count_pivot, x=brand_sentiment_count_pivot.index, y=selected_sentiment,
+    #               color_discrete_map={"positive": "green", "negative": "red"},
+    #               title="品牌情緒趨勢")
+    # fig = px.line(brand_sentiment_count_merged, x="artDate", y=selected_sentiment,
+    #               color_discrete_map={"positive": "green", "negative": "red"},
+    #               title="品牌情緒趨勢")
+
+    # fig.update_layout(
+    #     xaxis_title="月份",
+    #     yaxis_title="文章數量",
+    #     title="品牌情緒趨勢"
+    # )
+
+    # st.plotly_chart(fig)
+
+    # fig.update_layout(
+    #     xaxis_title="月份",
+    #     yaxis_title="文章數量",
+    #     title="品牌情緒趨勢"
+    # )
+    # st.plotly_chart(fig)
+
+
+
+    brand_sentiment_count_merged.rename(columns = {'positive_count':'positive', 'negative_count':'negative'}, inplace = True)
+    brand_sentiment_melt = pd.melt(brand_sentiment_count_merged, id_vars=['Brand', 'artDate'], value_vars=['positive', 'negative'])
+
+
+    # 以情緒為主 => 查看品牌
+    st.subheader("不同品牌間情緒比較")
+    sentiment_tabs = st.tabs(sentiment_list)
+    for i in range (len(sentiment_tabs)):
+        tmp_df = brand_sentiment_melt[brand_sentiment_melt['variable'] == sentiment_list[i]]
+        fig = px.line(tmp_df, x="artDate", y="value", color="Brand",title=sentiment_list[i])
+        sentiment_tabs[i].plotly_chart(fig, use_container_width=True)
+
+    st.sidebar.divider()
+    # 以品牌為主 => 查看正向情緒與負向情緒
+    st.subheader("品牌正負情緒比較")
+    brand_tabs = st.tabs(selected_brands)
+    for i in range (len(brand_tabs)):
+        # 篩選品牌
+        tmp_df = brand_sentiment_melt[brand_sentiment_melt['Brand'] == selected_brands[i]]
+        fig = px.line(tmp_df, x="artDate", y="value", color="variable",title=selected_brands[i])
+                #   color_discrete_map={"positive": "green", "negative": "red"},
+        brand_tabs[i].plotly_chart(fig, use_container_width=True)
+        brand_tabs[i].dataframe(brand_sentiment_count_merged[brand_sentiment_count_merged['Brand'] == selected_brands[i]], use_container_width=True)
 else:
-    df_filtered_sentiment_positive = pd.DataFrame(columns=df_select.columns)  # 空的 DataFrame
-
-if 'negative' in selected_sentiment:
-    df_filtered_sentiment_negative = df_select[df_select['sentimentRatio'] < 0.4]
-else:
-    df_filtered_sentiment_negative = pd.DataFrame(columns=df_select.columns)  # 空的 DataFrame
-
-df_filtered_sentiment_positive['artDate'] = pd.to_datetime(df_filtered_sentiment_positive['artDate'])
-df_filtered_sentiment_negative['artDate'] = pd.to_datetime(df_filtered_sentiment_negative['artDate'])
-
-# Group by brand, artDate, and sentiment, then calculate total volume
-brand_sentiment_count_positive = df_filtered_sentiment_positive.groupby(['Brand', pd.Grouper(key='artDate', freq='M', sort=False)])['Brand'].count().reset_index(name='count').rename(columns={'count': 'positive_count'})
-
-brand_sentiment_count_negative = df_filtered_sentiment_negative.groupby(['Brand', pd.Grouper(key='artDate', freq='M', sort=False)])['Brand'].count().reset_index(name='count').rename(columns={'count': 'negative_count'})
-
-
-# Merge positive and negative counts
-brand_sentiment_count_merged = pd.merge(brand_sentiment_count_positive, brand_sentiment_count_negative, on=['Brand', 'artDate'], how='outer').fillna(0)
-# # Pivot the table to have sentimentRatio as columns
-# brand_sentiment_count_pivot = brand_sentiment_count_merged.pivot_table(index='artDate',
-#                                                                       columns='Brand',
-#                                                                       values='count',
-#                                                                       fill_value=0)
-
-# # Plot line chart
-# fig = px.line(brand_sentiment_count_pivot, x=brand_sentiment_count_pivot.index, y=selected_sentiment,
-#               color_discrete_map={"positive": "green", "negative": "red"},
-#               title="品牌情緒趨勢")
-# fig = px.line(brand_sentiment_count_merged, x="artDate", y=selected_sentiment,
-#               color_discrete_map={"positive": "green", "negative": "red"},
-#               title="品牌情緒趨勢")
-
-# fig.update_layout(
-#     xaxis_title="月份",
-#     yaxis_title="文章數量",
-#     title="品牌情緒趨勢"
-# )
-
-# st.plotly_chart(fig)
-
-# fig.update_layout(
-#     xaxis_title="月份",
-#     yaxis_title="文章數量",
-#     title="品牌情緒趨勢"
-# )
-# st.plotly_chart(fig)
-
-
-
-brand_sentiment_count_merged.rename(columns = {'positive_count':'positive', 'negative_count':'negative'}, inplace = True)
-brand_sentiment_melt = pd.melt(brand_sentiment_count_merged, id_vars=['Brand', 'artDate'], value_vars=['positive', 'negative'])
-
-
-# 以情緒為主 => 查看品牌
-st.subheader("不同品牌間情緒比較")
-sentiment_tabs = st.tabs(sentiment_list)
-for i in range (len(sentiment_tabs)):
-    tmp_df = brand_sentiment_melt[brand_sentiment_melt['variable'] == sentiment_list[i]]
-    fig = px.line(tmp_df, x="artDate", y="value", color="Brand",title=sentiment_list[i])
-    sentiment_tabs[i].plotly_chart(fig, use_container_width=True)
-
-st.sidebar.divider()
-# 以品牌為主 => 查看正向情緒與負向情緒
-st.subheader("品牌正負情緒比較")
-brand_tabs = st.tabs(selected_brands)
-for i in range (len(brand_tabs)):
-    # 篩選品牌
-    tmp_df = brand_sentiment_melt[brand_sentiment_melt['Brand'] == selected_brands[i]]
-    fig = px.line(tmp_df, x="artDate", y="value", color="variable",title=selected_brands[i])
-            #   color_discrete_map={"positive": "green", "negative": "red"},
-    brand_tabs[i].plotly_chart(fig, use_container_width=True)
-    brand_tabs[i].dataframe(brand_sentiment_count_merged[brand_sentiment_count_merged['Brand'] == selected_brands[i]], use_container_width=True)
+    st.error("請至少選擇一項品牌與情緒")
