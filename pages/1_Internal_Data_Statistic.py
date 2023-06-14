482c0a3429e9a06ec6901e347698e3d7cc5ce764
JamesLee2324 <jameslee2342324@gmail.com>
2023-06-14 15:15:59 +0800

    debug iloc little2

--- pages/1_Internal_Data_Statistic.py
+++ pages/1_Internal_Data_Statistic.py
@@ -33,14 +33,14 @@ df_interact['交車日'] = pd.to_datetime(df_interact['交車日'],format='%Y-%m
 
 # 新增kicks, sentra, 其他
 df_interact['KicksSentra'] = '無試乘/交車'
-df_interact.loc[(df_interact['成交車系'] == 'P15') |
-                (df_interact['試乘_P15'] >= 1), 'KicksSentra'] = 'Kicks'
+condition = (df_interact['成交車系'] == 'P15') | (df_interact['試乘_P15'] >= 1)
+df_interact.loc[condition, 'KicksSentra'] = 'Kicks'
 
-df_interact.loc[(df_interact['成交車系'] == 'B18') |
-                (df_interact['試乘_B18'] >= 1), 'KicksSentra'] = 'Sentra'
+condition = (df_interact['成交車系'] == 'B18') | (df_interact['試乘_B18'] >= 1)
+df_interact.loc[condition, 'KicksSentra'] = 'Sentra'
 
-df_interact.loc[(df_interact['成交車系'] != 'NULL') |
-                (df_interact['試乘車輛'] != 0), 'KicksSentra'] = '試乘/交車其他車系'
+condition = (df_interact['成交車系'] != 'NULL') | (df_interact['試乘車輛'] != 0)
+df_interact.loc[condition, 'KicksSentra'] = '試乘/交車其他車系'
 
 # Set header title
 st.title("Nissan 內部資料統計")
