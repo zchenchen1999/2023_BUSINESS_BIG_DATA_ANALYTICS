14a2f98e6782fbc6e885e310b915d001c831982a
larrytherealboy <abclarryabclarry@gmail.com>
2023-06-15 18:35:58 +0800

    update correlation page

--- pages/7_Correlation.py
+++ pages/7_Correlation.py
@@ -92,7 +92,7 @@ elif optionFrequency == '週頻':
       index=data[date_int].tolist().index(default_value))
 
 # 防呆機制：結束月份不能選擇比起始月份還前面的日期
-if optionStartTime < optionStartTime:
+if int(optionStartTime) < int(optionStartTime):
     st.sidebar.error("結束月份不能早於起始月份")
 
 # 給出對應的表格
