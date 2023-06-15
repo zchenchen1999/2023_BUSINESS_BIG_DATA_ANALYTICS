6084419ef303558eaa3819770e6039df88d4a58a
JamesLee2324 <jameslee2342324@gmail.com>
2023-06-15 20:17:09 +0800

    debug

--- pages/3_Word_frequency.py
+++ pages/3_Word_frequency.py
@@ -65,8 +65,7 @@ Sentra = ['Sentra', 'sentra', '仙草', 'B18']
 st.sidebar.subheader('參數調整')
 selected_brands = st.sidebar.multiselect('選擇品牌', brand_list, default=['Nissan'])
 
-default_index3 = car_list.index("全部車系")
-selected_car = st.sidebar.selectbox('選擇車系', car_list, index=default_index3)
+
 
 # st.sidebar.divider()  # 分隔線
 
@@ -101,6 +100,9 @@ if selected_ending_date < selected_beginning_date:
     st.sidebar.error("結束月份不能早於起始月份")
 
 if selected_brands == 'Nissan':
+    
+    default_index3 = car_list.index("全部車系")
+    selected_car = st.sidebar.selectbox('選擇車系', car_list, index=default_index3)
 
     if selected_car == '全部車系':
 
@@ -111,7 +113,7 @@ if selected_brands == 'Nissan':
         
     elif selected_car == 'Kicks':
 
-        select_id2 = df_interact['words'].apply(lambda x: any(item in x for item in Kicks))
+        select_id2 = [any(item in x for item in Kicks) for x in df_interact['words']]
         df_select = df_interact[select_id2]
 
         df_select = df_select.loc[(df_select['brand'].isin(list(selected_brands))) &
@@ -120,12 +122,12 @@ if selected_brands == 'Nissan':
         
     elif selected_car == 'Sentra':
 
-        select_id2 = df_interact['words'].apply(lambda x: any(item in x for item in Sentra))
+        select_id2 = [any(item in x for item in Sentra) for x in df_interact['words']]
         df_select = df_interact[select_id2]
 
         df_select = df_select.loc[(df_select['brand'].isin(list(selected_brands))) &
-                                (df_select['artDate'].dt.to_period('M') >= selected_beginning_date.to_period('M')) &
-                                (df_select['artDate'].dt.to_period('M') <= selected_ending_date.to_period('M'))]
+                                  (df_select['artDate'].dt.to_period('M') >= selected_beginning_date.to_period('M')) &
+                                  (df_select['artDate'].dt.to_period('M') <= selected_ending_date.to_period('M'))]
         
 else:
     df_select = df_interact.loc[(df_interact['brand'].isin(list(selected_brands))) &
